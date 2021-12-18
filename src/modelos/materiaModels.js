3932aa04904ab5d7e94502db8eccbb1ffef1ec0c
Yessid Rios <91134063+YessidR@users.noreply.github.com>
2021-12-18 11:58:15 -0500

    Base de datos en la nube

--- src/modelos/materiaModels.js
+++ src/modelos/materiaModels.js
@@ -1,34 +1,32 @@
-const mongoose = require("mongoose");
-const {Schema,model}=mongoose;
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
 
 const materiaSchema = new Schema({
-    nombre:{
-        type:"string",
-        required:true,
-    },
-    descripcion:{
-        type:"string",
-        required:true,
-    },
-    cod:{
-        type:"string",
-        required:true,
-        unique:true,
-    },
-    unidad_medida:{
-        type:"string",
-        required:true,
-    },
-    cantidad_disponible:{
-        type:"number",
-        required:true,
-    },
-    valor_unitario:{
-        type:"number",
-        required:true,
-    }
-
+  nombre: {
+    type: 'string',
+    required: true,
+  },
+  descripcion: {
+    type: 'string',
+    required: true,
+  },
+  cod: {
+    type: 'string',
+    required: true,
+    unique: true,
+  },
+  unidad_medida: {
+    type: 'string',
+    required: true,
+  },
+  cantidad_disponible: {
+    type: 'number',
+    required: true,
+  },
+  valor_unitario: {
+    type: 'number',
+    required: true,
+  },
 });
-const materiaModels= model("materias",materiaSchema);
-exports.materiaModels=materiaModels;
-
+const materiaModels = model('materias', materiaSchema);
+exports.materiaModels = materiaModels;
--- src/modelos/registerModel.js
+++ src/modelos/registerModel.js
@@ -1,34 +1,33 @@
-const mongoose = require("mongoose");
-const {Schema,model}=mongoose;
-const {genSalt,hash}=require("bcrypt");
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+const { genSalt, hash } = require('bcrypt');
 
 const registerSchema = new Schema({
-    nombre:{
-        type:"string",
-        required:true,
-    },
-    user:{
-        type:"string",
-        required:true,
-    },
-    password:{
-        type:"string",
-        required:true,
-    },
-    email:{
-        type:"string",
-        required:true,
-    },
-    cargo:{
-        type:"string",
-        required:true,
-    }
-
+  nombre: {
+    type: 'string',
+    required: true,
+  },
+  user: {
+    type: 'string',
+    required: true,
+  },
+  password: {
+    type: 'string',
+    required: true,
+  },
+  email: {
+    type: 'string',
+    required: true,
+  },
+  cargo: {
+    type: 'string',
+    required: true,
+  },
 });
-registerSchema.pre("save", async function (next) {
-    const salt = await genSalt(10);
-    this.password = await hash(this.password, salt);
-    next();
+registerSchema.pre('save', async function (next) {
+  const salt = await genSalt(10);
+  this.password = await hash(this.password, salt);
+  next();
 });
-const registerModel= model("usuarios",registerSchema);
-exports.registerModel=registerModel;
+const registerModel = model('usuarios', registerSchema);
+exports.registerModel = registerModel;
